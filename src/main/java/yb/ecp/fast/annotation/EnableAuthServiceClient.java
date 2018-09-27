d3b92b5d3a2c3d9826c7de610945f140de6e0438
tigerwer@126.com <wangyidan1>
2018-09-27 15:49:47 +0800

    debug

--- src/main/java/yb/ecp/fast/annotation/EnableAuthServiceClient.java
+++ src/main/java/yb/ecp/fast/annotation/EnableAuthServiceClient.java
@@ -1,16 +1,17 @@
 package yb.ecp.fast.annotation;
 
-import java.lang.annotation.Annotation;
+import org.springframework.context.annotation.Import;
+import yb.ecp.fast.feign.AuthClient;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.springframework.context.annotation.Import;
-import yb.ecp.fast.feign.AuthClient;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({java.lang.annotation.ElementType.TYPE})
 @Import({AuthClient.class})
-public @interface EnableAuthServiceClient {}
+public @interface EnableAuthServiceClient {
+}
 
 
 /* Location:              D:\maven-snapshots\auth-server-client-0.0.3-20180920.181922-306.jar!\yb\ecp\fast\annotation\EnableAuthServiceClient.class
